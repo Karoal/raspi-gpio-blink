60d2454ecf811873ffb5c24c89aa4be34761bd4a
Karol Ciszek <karol.j.ciszek@gmail.com>
2014-01-25 18:51:15 +0000

    first

--- raspi-gpio-blink.py
+++ raspi-gpio-blink.py
@@ -9,22 +9,30 @@ gpio.setup(13, gpio.OUT)
 gpio.setup(15, gpio.OUT)
 
 counter = 0             # LEDs turn on/off based on whether counter is a multiple of a certain num
-delay = 60/(2*128)      # argument for time.sleep, delay between each while loop
+delay = 60/(4*128)      # argument for time.sleep, delay between each while loop
 toggle = {11: False,   # bool values for toggling
           13: False,
           15: False}
 
 while True:
-    toggle[11] = not toggle[11]
-    gpio.output(11, toggle[11])
+    if counter % 2 != 0:
+        gpio.output(11, 0)
+        gpio.output(13, 0)
+        gpio.output(15, 0)
 
     if counter % 2 == 0:
-        toggle[13] = not toggle[13]
-        gpio.output(13, toggle[13])
+        toggle[11] = not toggle[11]
+        gpio.output(11, 1)
 
     if counter % 4 == 0:
+        gpio.output(13, 1)
+
+    if counter % 8 == 0:
         toggle[15] = not toggle[15]
-        gpio.output(15, toggle[15])
+        gpio.output(15, 1)
 
     time.sleep(delay)
+
     counter += 1
+    if counter > 8:
+        counter = 0
