97e166e2d95249c2db75819f59824ecd79e0c65a
Karol Ciszek <karol.j.ciszek@gmail.com>
2014-01-26 02:06:57 +0000

    optimised

--- raspi-gpio-fala.py
+++ raspi-gpio-fala.py
@@ -4,7 +4,6 @@
 import RPi.GPIO as gpio
 import time
 
-
 gpio.setmode(gpio.BOARD)
 gpio.setup(11, gpio.OUT)
 gpio.setup(13, gpio.OUT)
@@ -12,6 +11,16 @@ gpio.setup(15, gpio.OUT)
 
 counter = 0             # LEDs turn on/off based on whether counter is a multiple of a certain num
 delay = 60/(4*128)      # argument for time.sleep, delay between each while loop
+
+hour, minute = 6, 45    # time at which LEDs toggle fastest
+
+waketime = time.strptime(str(hour) + " " + str(minute), "%H %M")
+print(waketime)
+
+
+def calc_time(t):
+    """ Returns the appropriate delay for time.sleep """
+    return (time.mktime(t) % 3600*24) / 3600*12
 
 while True:
     gpio.output(11, 1 if counter == 0 else 0)
@@ -21,5 +30,7 @@ while True:
 
     if counter >= 3:
         counter = 0
+        delay = calc_time(waketime)
+        print(delay)
     else:
         counter += 1
