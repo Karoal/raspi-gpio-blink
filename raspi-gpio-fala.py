198b76011107ba3995c184fcaa6eb32db0c92a5a
Karol Ciszek <karol.j.ciszek@gmail.com>
2014-01-26 22:12:36 +0000

    fala updated

--- raspi-gpio-fala.py
+++ raspi-gpio-fala.py
@@ -37,6 +37,6 @@ while True:
     if counter >= 3:
         counter = 0
         delay = calc_time(hour, minute)
-        print(delay)
+        print("sec: {0:25} per min: {1}".format(1 / delay, 60 / delay))
     else:
         counter += 1
