041c4306d76219bdbb00fa7310e389ba0e626418
Karol Ciszek <karol.j.ciszek@gmail.com>
2014-02-01 17:25:37 +0000

    test added

---
+++ test.py
@@ -0,0 +1,19 @@
+# Test to see if 8 LEDs work
+
+import RPi.GPIO as gpio
+
+pins = (7, 11, 12, 13, 15, 16, 18, 22)
+
+gpio.setmode(gpio.BOARD)
+gpio.setup( 7, gpio.OUT)
+gpio.setup(11, gpio.OUT)
+gpio.setup(12, gpio.OUT)
+gpio.setup(13, gpio.OUT)
+gpio.setup(15, gpio.OUT)
+gpio.setup(16, gpio.OUT)
+gpio.setup(18, gpio.OUT)
+gpio.setup(22, gpio.OUT)
+
+for pin in pins:
+    gpio.output(pin, 1)
+    time.sleep(0.5)
